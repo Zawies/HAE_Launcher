2a3b4bf9942daf45ecccfe78d8509e3311c508ef
Jakub Zarzycki <P2503108@my365.dmu.ac.uk>
2022-03-14 18:46:28 +0000

    added battery indicator TextView

--- app/src/main/java/uk/co/dooapp/hae_launcher/MainActivity.java
+++ app/src/main/java/uk/co/dooapp/hae_launcher/MainActivity.java
@@ -2,8 +2,11 @@ package uk.co.dooapp.hae_launcher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        addAppListListener();
+    }
+
+    private void addAppListListener()
+    {
+        ImageButton appList = findViewById(R.id.appList);
+        appList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
