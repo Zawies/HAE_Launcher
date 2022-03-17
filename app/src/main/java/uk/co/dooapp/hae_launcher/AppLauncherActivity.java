a8684ec3e7f8b8e70a9b00cb0c0a409d2a8693cf
Jakub Zarzycki <P2503108@my365.dmu.ac.uk>
2022-03-17 11:14:49 +0000

    added AppLauncherActivity

---
+++ app/src/main/java/uk/co/dooapp/hae_launcher/AppLauncherActivity.java
@@ -0,0 +1,14 @@
+package uk.co.dooapp.hae_launcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppLauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_launcher);
+    }
+}
